753ef9893d3d4fea50a0c83d1c904fa6dd40fae7
Federico Barrios <fbarrios@fi.uba.ar>
2014-10-12 16:47:07 -0300

    Graph created.

--- textrank.py
+++ textrank.py
@@ -1,14 +1,31 @@
 
+from pygraph.classes.graph import graph as pygraph
+
 TEST_FILE = "testdata/textrank_example.txt"
 
 
-
-
-
 def textrank(filename):
-    pass
+    graph = get_graph(filename)
+
+    set_graph_edge_weights(graph)
 
 
+def get_graph(filename):
+    graph = pygraph()
+
+    # Creates the graph.
+    with open(filename) as fp:
+        for line in fp:
+            graph.add_node(line)
+
+    graph.complete()
+
+    return graph
+
+
+def set_graph_edge_weights(graph):
+    for node in graph.nodes():
+        print node
 
 
 textrank(TEST_FILE)
